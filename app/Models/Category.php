c2d00741dd9a22782ece612e8f82c3c98b0b3164
FarajElmadhi <modahhy@gmail.com>
2021-10-21 14:01:31 +0000

    ...

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,15 +2,34 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Carbon\Carbon;
 
 // Auto Models By Baboon Script
 // Baboon Maker has been Created And Developed By  [it v 1.6.32]
 // Copyright Reserved  [it v 1.6.32]
 class Category extends Model {
-	use SoftDeletes;
+   use HasFactory;
+
+	use SoftDeletes;
 	protected $dates = ['deleted_at'];
 
 protected $table    = 'categories';
+
+ //  return created_at date format readable ---->
+ public function getCreatedAtAttribute($value)
+ {
+     $date = Carbon::parse($value);
+     return $date->format('Y-m-d H:i:s');
+ }
+
+//  return updated_at date format readable ---->
+ public function getUpdatedAtAttribute($value)
+ {
+     $date = Carbon::parse($value);
+     return $date->format('Y-m-d H:i:s');
+ }
 protected $fillable = [
 		'id',
 		'admin_id',
